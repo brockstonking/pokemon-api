e138879ac39710a2874084786b7ef441253c11a8
unknown <honorpurpose@gmail.com>
2019-04-16 04:52:58 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
+
+
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      image: null,
+      name: '', 
+      pokemonId: 2
+    }
+
+    this.handleClick = this.handleClick.bind( this )
+    this.getNext = this.getNext.bind( this )
+  }
+
+  
+
+  handleClick(id){
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`).then(results => {
+      this.setState({
+        image: results.data.sprites.front_default,
+        name: results.data.name
+      })
+    })
+  }
+
+  getNext(){
+    this.setState({
+      pokemonId: this.state.pokemonId + 1
+    })
+    this.handleClick(this.state.pokemonId)
+  }
+
   render() {
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='display'>
+      <img src={this.state.image} alt="Click >> to get Pokemon!"/>
+        <h1>{this.state.name}</h1>
+      </div>
+        
+      <div> 
+        <div className='buttons' onClick={() => this.handleClick(this.state.pokemonId)}>Click to get pokemon</div>
+        <div className='buttons' onClick={this.getNext}>Next Pokemon</div>
+      </div>
       </div>
     );
   }
