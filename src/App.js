482116c1fed6378eccbd74f11414b39047894095
unknown <honorpurpose@gmail.com>
2019-04-16 06:30:04 -0600

    working

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
     this.state = {
       image: 'https://fontmeme.com/images/Pokemon-Logo.jpg',
       name: '', 
-      pokemonId: 2
+      pokemonId: 1,
+      xp: null
     }
 
     this.handleClick = this.handleClick.bind( this )
@@ -24,12 +25,14 @@ class App extends Component {
     axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`).then(results => {
       this.setState({
         image: results.data.sprites.front_default,
-        name: results.data.name
+        name: results.data.name,
+        xp: results.data.base_experience
       })
     })
   }
 
   getNext(){
+    
     this.setState({
       pokemonId: this.state.pokemonId + 1
     })
@@ -43,6 +46,7 @@ class App extends Component {
       <div className='display'>
       <img className='image' src={this.state.image} alt="Click >> to get Pokemon!"/>
         <h1>{this.state.name}</h1>
+        <h3>XP: {this.state.xp}</h3>
       </div>
         
       <div> 
